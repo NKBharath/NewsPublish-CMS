63de11d0114683b68d16928b99a6ef524b1e0a42
Harsine07 <harsineku.22aim@kongu.edu>
2025-05-06 11:34:03 +0530

    publisher model modified

--- server/models/publisher.model.js
+++ server/models/publisher.model.js
@@ -21,9 +21,25 @@ const publisherSchema = new mongoose.Schema({
     category:{
         type: String,
         required: true,
+    },
+    mobileNumber:{
+        type: String,
+        required: true,   
+    },
+    bio:{
+        type:String,
+        required: true,
+        maxlength:200,
+    },
+    joinedOn:{
+        type: Date,
+        required: true,
+        default: Date.now,
     }
+    
 
-})
+},
+{ timestamps: true });
 
 const PublisherModel = mongoose.model('publishers', publisherSchema);
 module.exports = PublisherModel;
